da2a5b5a4ab0c3ab9906e9c9fa6aee2615bc88eb
MaxTeselkin <tesmaxxxx@gmail.com>
2023-07-28 19:37:53 +0400

    experiment

--- supervisely_integration/serve/src/main.py
+++ supervisely_integration/serve/src/main.py
@@ -54,14 +54,10 @@ class XMemTracker(MaskTracking):
         # load processor
         processor = InferenceCore(self.model, config=self.config)
         processor.set_all_labels(range(1, num_objects))
-        # preprocess input mask
-        input_mask = index_numpy_to_one_hot_torch(input_mask, num_objects)
-        input_mask = input_mask[1:]
-        input_mask = input_mask.to(self.device)
         results = []
         # track input objects' masks
         with torch.cuda.amp.autocast(enabled=True):
-            print("using autocast...")
+            print("second autocast experiment...")
             for i, frame in enumerate(frames):
                 # preprocess frame
                 frame = frame.transpose(2, 0, 1)
@@ -69,6 +65,10 @@ class XMemTracker(MaskTracking):
                 frame = im_normalization(frame)
                 # inference model on specific frame
                 if i == 0:
+                    # preprocess input mask
+                    input_mask = index_numpy_to_one_hot_torch(input_mask, num_objects)
+                    input_mask = input_mask[1:]
+                    input_mask = input_mask.to(self.device)
                     print(f"frame is cuda: {frame.is_cuda}")
                     print(f"mask is cuda: {input_mask.is_cuda}")
                     prediction = processor.step(frame, input_mask)
