bafdbe95a65c86db8237ba60ddb8eb169365dc3f
MaxTeselkin <tesmaxxxx@gmail.com>
2023-07-31 18:19:52 +0400

    experiment

--- supervisely_integration/serve/src/main.py
+++ supervisely_integration/serve/src/main.py
@@ -74,7 +74,7 @@ class XMemTracker(MaskTracking):
                 frame = frame.transpose(2, 0, 1)
                 frame = torch.from_numpy(frame)
                 frame = torch.unsqueeze(frame, 0)
-                frame = torch.nn.functional.interpolate(frame, (resized_height, resized_width), mode="bilinear")
+                frame = torch.nn.functional.interpolate(frame, (resized_height, resized_width), mode="nearest")
                 frame = frame.squeeze().numpy()
                 frame = torch.from_numpy(frame).float().to(self.device) / 255
                 frame = im_normalization(frame)
