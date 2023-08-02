c396130122c786fd663d6998a22eaee6b5bcea75
MaxTeselkin <tesmaxxxx@gmail.com>
2023-08-02 19:23:25 +0400

    update docker image

--- supervisely_integration/docker/publish.sh
+++ supervisely_integration/docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/xmem:1.0.1 . && \
-docker push supervisely/xmem:1.0.1
+docker build -t supervisely/xmem:1.0.2 . && \
+docker push supervisely/xmem:1.0.2
--- supervisely_integration/serve/src/main.py
+++ supervisely_integration/serve/src/main.py
@@ -74,7 +74,7 @@ class XMemTracker(sly.nn.inference.MaskTracking):
                 frame = frame.transpose(2, 0, 1)
                 frame = torch.from_numpy(frame)
                 frame = torch.unsqueeze(frame, 0)
-                frame = torch.nn.functional.interpolate(frame, (resized_height, resized_width), mode="nearest")
+                frame = torch.nn.functional.interpolate(frame, (resized_height, resized_width), mode="bilinear")
                 frame = frame.squeeze()
                 frame = frame.float().to(self.device) / 255
                 frame = im_normalization(frame)
